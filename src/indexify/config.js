df765a49dc713df309addc3a061374b0ec11af23
Vaasu Dhand <dhandvaasu@gmail.com>
2021-04-29 15:27:41 -0400

    Added NPX Prefix

--- src/indexify/config.js
+++ src/indexify/config.js
@@ -1,12 +1,12 @@
 const createJSIndex = {
-  cmd: 'create-index',
+  cmd: 'npx create-index',
   path: '',
   flags: ['--extensions js jsx'], // indexes JS and JSX files
   extension: "js"
 };
 
 const createTSIndex = {
-  cmd: 'create-ts-index create -w -b',  // * cti create does not work with npx
+  cmd: 'npx create-ts-index create -w -b',  // * cti create does not work with npx
   path: '',
   flags: [], // indexes TS and TSX files
   extension: "ts"
